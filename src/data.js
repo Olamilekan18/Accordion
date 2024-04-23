e7037a3ae3a3bff2cbb7dd3709f0e32a52fcf359
Kareem Olamilekan <olamilekankareem717@gmail.com>
2024-04-23 06:46:19 +0100

    continued

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data =[
+    {
+        id: '1',
+        question: "What's your name?",
+        answer: 'Olamilekan'
+    },
+    {
+        id: '2',
+        question: 'Nationality',
+        answer: 'Nigeria'
+    },
+    {
+        id: '3',
+        question: 'Date of birth',
+        answer: 'December 18'
+    },
+    {
+        id:'4',
+        question: 'Favorite food',
+        answer: 'Beans'
+    }
+]
+
+export default data
